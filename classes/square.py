62f769fd66a35237a9061558ba324da27f37e22d
mahdi-mallaee <mahdi.mallaee@gamil.com>
2023-04-24 23:22:25 +0430

    optimizing get piece image function

--- classes/square.py
+++ classes/square.py
@@ -13,25 +13,21 @@ class Square:
         self.piece_notation = piece_notation
 
     def get_piece_image(self):
-        color = "white"
+        color = self.piece_notation[0]
         notation = self.piece_notation[1]
-        if self.piece_notation[0] == 'b':
-            color = 'black'
-
-        if notation == 'r':
-            piece_image = pygame.image.load('assets/' + color + '_rook.png')
-        elif notation == 'n':
-            piece_image = pygame.image.load('assets/' + color + '_knight.png')
-        elif notation == 'b':
-            piece_image = pygame.image.load('assets/' + color + '_bishop.png')
-        elif notation == 'q':
-            piece_image = pygame.image.load('assets/' + color + '_queen.png')
-        elif notation == 'k':
-            piece_image = pygame.image.load('assets/' + color + '_king.png')
-        elif notation == 'p':
-            piece_image = pygame.image.load('assets/' + color + '_pawn.png')
-
-        return piece_image
+        color_dict = {
+            'b': 'black',
+            'w': 'white'
+        }
+        notation_dict = {
+            'r': 'rook',
+            'n': 'knight',
+            'b': 'bishop',
+            'q': 'queen',
+            'k': 'king',
+            'p': 'pawn'
+        }
+        return pygame.image.load('assets/' + color_dict[color] + '_' + notation_dict[notation] + '.png')
 
     def get_legal_moves(self):
         pos = self.pos
